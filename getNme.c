40b02db8fb5d97ea2acfa98221276d7a2b72ba3e
cuterinrin <yro7045@naver.com>
2018-11-20 23:31:41 +0900

    get number me 구축

---
+++ getNme.c
@@ -0,0 +1,25 @@
+int get_number_me(int frm) {
+        
+int num;
+int retry;
+    
+do 
+{
+	retry = 0;//초기화  
+
+    printf("1에서 %i 사이의 수를 입력하시오. : ", N*N);
+    scanf("%i", &num);
+        if (num < 1 || num > N*N) 
+		{// 다시 입력받아야 함  
+        retry = 1;                     
+        }
+    }
+}while (retry == 1); //retry=1이면 do 다시돌리기 
+                        
+printf("%i를 선택했습니다. \n", num);   
+
+
+return number;
+}
+
+
