bc70d10a6c198755ab69f747c170dd6082becec1
cuterinrin <yro7045@naver.com>
2018-11-21 09:06:11 +0900

    process bingo 구축

--- process_bingo.c
+++ process_bingo.c
@@ -1,3 +1,35 @@
-int process_bingo;
+int process_bingo(int num);
+/*필요한 것
+1. get number에서 입력받은 숫자를 -1로 바꾸는 기능
+2. for문에서 입력 숫자=칸에 들어있는 숫자 면 -1로 값 바꾸기  
+
+*/
+
+int i,j;
+
+for(i=0; i<N; i++)
+{
+	for(j=0; j<N; j++)
+	{
+		if( num == bingo_user[i][j])	//입력 숫자 = 빙고 숫자			
+		{
+			bingo_user[i][j] = '-1';	
+		}
+	}
+}
+
+for(i=0; i<N; i++)
+{
+	for(j=0; j<N; j++)
+	{
+		if( num_com == bingo_computer[i][j])				
+		{
+			bingo_computer[i][j] = '-1';				
+		}
+	}
+}
+
+return 0;
+}
 
 
